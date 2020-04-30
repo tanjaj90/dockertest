516d703dd39f9b7ed1659a1b7aff8bb2b57b4f53
tanja <tanja.jovovic@crossmasters.com>
2020-04-30 15:30:44 +0200

    change added

--- example1.js
+++ example1.js
@@ -1,7 +1,7 @@
 //example 1
 async function main() {
       
-    console.log("Hello");
+    console.log("Helloooooo");
     setTimeout(() => { console.log("World!"); }, 30000);
     console.log("Goodbye!");
     
